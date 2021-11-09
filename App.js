85ff4a5728d2c173c6803f545a5c2374bfb60a4c
John Tierney <91153258+johnjohntierney@users.noreply.github.com>
2021-11-09 18:10:12 +0000

    My current Pizza App
    My current Pizza App

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import * as React from 'react'
-import { View, Text, StyleSheet, Image, toucho, } from 'react-native'
+import { Text, Image, } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NativeBaseProvider, Button, Col, Input, Container, Row, Grid, Heading } from 'native-base'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider, Button, Input, Container, Item, Heading, H1 } from 'native-base'
+import { Col, Row, Grid } from 'react-native-easy-grid'
 import logo from './assets/pizza.jpg'
+
+
 
 //This section is complete
 function HomeScreen({ navigation }) {
@@ -17,11 +20,42 @@ function HomeScreen({ navigation }) {
   return (
     <Container>
         <Grid>
+          <Row>
             <Col style={{alignItems: 'center', justifyContent: 'center', backgroundColor: 'olive'}}>
              <Image source={logo} style={{ width: 280, height: 180 }} /> 
+            </Col> 
+            </Row>
+            <Row>
+            <Col style={{alignItems: 'center', justifyContent: 'center', padding: 20}}>
+              <H1>Mario's Pizza</H1>
+              <Text>The best pizza in Belfast</Text>
+              </Col>
+              </Row>
+              <Row>
+              <Col style={{alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: 'orangered'}}>
+              <Item regular>
+                <Input 
+                  placeholder='Email address' 
+                  backgroundColor='white'
+                  value={text} 
+                  onChangeText={text => setText(text)}
+                  />
+                  </Item>
+                  <Button onPress={() => navigation.navigate('Welcome', {myEmail: text})} block light>
+                <Text>Get updates from Mario's Pizza</Text>
+              </Button>
             </Col>
-            </Grid>
+          </Row>
+        </Grid>
+
+            
+
+
+          
+  
           </Container>
+
+          
   );
 }
     
