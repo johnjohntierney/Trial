d9a7a5c9d1bcb471a4b4d2f38c1ffee2847d591b
John Tierney <91153258+johnjohntierney@users.noreply.github.com>
2021-11-16 13:49:10 +0000

    Working Pizza app
    Email issue to be resolved

--- App.js
+++ App.js
@@ -1,55 +1,74 @@
-import * as React from 'react'
-import { Text, StyleSheet, Image, } from 'react-native'
-
-
-import { NativeBaseProvider, Button, Input, Container, Item, Heading, } from 'native-base'
+import * as React from 'react';
+import { Button, Text, Image, View, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider, Input, Container, Heading, } from 'native-base'
 import { Col, Row, Grid } from "react-native-easy-grid"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import logo from './assets/pizza.jpg'
 
 
-function App() {
+function HomeScreen({ navigation }) {
+  const [userEmail, setEmailAddress] = React.useState('');
   return (
-      <NativeBaseProvider>
-        <Grid>
-          <Row style={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#008c45'}}>
-             <Image source={logo} style={{ width: 280, height: 180 }} /> 
-            </Row>
-            <Row style={{alignItems: 'center', justifyContent: 'center', padding: 20}}>
-              <Heading size="xl">Mario's Pizza</Heading>
-              <Text size="lg">The best pizza in Belfast</Text>
-              </Row>
-              
-              <Row style={{alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: '#cd212a'}}>
-              <Input 
-                  placeholder='Email address' 
-                  style={ styles.textBox }
-                  />
-                  <Button 
-                  onPress={() => navigation.navigate('Welcome', {myName: Text, MyName: Text})} block bordered primary>
-                <Text>Get updates from Mario's Pizza</Text>
-              </Button>
-          </Row>
-        </Grid>
-        </NativeBaseProvider>
-          
+    <NativeBaseProvider>
+      <Grid>
+        <Row>
+    <Col style={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#008c45'}}>
+           <Image source={logo} style={{ width: 280, height: 180 }} /> 
+    </Col>
+        </Row>
+
+    <Col style={{alignItems: 'center', justifyContent: 'center', padding: 20}}>
+      <Heading size="xl">Mario's Pizza</Heading>
+        <Text size="lg">The best pizza in Belfast</Text>
+    </Col>
+
+    <Col style={{alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: '#cd212a'}}>
+        <Input 
+          placeholder='Email address' 
+          backgroundColor='white'
+          onChangeText={ email => setEmailAddress(email) } />
+                  
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Get updates from Mario's Pizza"
+          onPress={() => navigation.navigate('Welcome', {})}block bordered primary></Button>
+    </View>
+    </Col>
+    </Grid>
+    </NativeBaseProvider>
   );
 }
 
-    
-const styles = StyleSheet.create({
-  textBox: {
-    backgroundColor: 'white',
-    width: 250,
-    marginBottom: 5,
-  },
-  button: {
-    backgroundColor: '#D3D3D3',
-    width: 250,
-  },
-  image: {
-    width: 405, 
-    height: 259,
-  },
-})
+function WelcomeScreen({ navigation }) {
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#cd212a' }}>
+      <Button
+        title="Welcome to Mario's
+        We will send updates to { userEmail }"
+        onPress={() => navigation.navigate('Welcome', {userEmail: Text, MyName: Text})}block bordered primary>
+      </Button>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 export default App;
+
+
+
+
